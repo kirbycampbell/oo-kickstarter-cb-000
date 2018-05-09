eed87a33f7d78b0ddd4e9d11420a4c64049c12b1
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-09 23:09:06 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,9 +9,6 @@ attr_accessor :name, :backed_projects
 
   def back_project(project)
     @backed_projects << project
-    projectnew = Project.new(name)
-    @backed_projects << projectnew
-    project.backer = self
   end
 
 end
